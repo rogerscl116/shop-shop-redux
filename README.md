3f5ffee462453e2171b8978f30eb79424fbf8da5
Charity Rogers <rogerscl116@gmail.com>
2021-09-16 08:33:58 -0500

    initial files and redux packages

---
+++ README.md
@@ -0,0 +1,33 @@
+# Shop Shop
+
+## License
+<a href="https://opensource.org/licenses/MIT"><img src="https://img.shields.io/badge/License-MIT-yellow" alt="MIT license badge"/></a>
+
+## Description
+
+Deployed application on heroku: https://shop-shop03718349.herokuapp.com/
+
+### Screenshots
+![Shop Shop Home]()
+
+
+## Table of Contents
+ * [Installation](#installation)
+ * [Usage](#usage)
+ * [Contributing](#contributing)
+ * [Questions](#questions)
+        
+## Installation
+Clone this repository and enter `npm install` in the terminal to install the necessary dependencies.
+   
+## Usage
+
+
+## Contributing
+Charity Rogers
+
+## Questions
+If you have any questions, please contact me!
+
+  - GitHub: https://github.com/rogerscl116
+  - Email: rogerscl116@gmail.com 
---
+++ client/src/App.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+import Home from './pages/Home';
+import Detail from './pages/Detail';
+import NoMatch from './pages/NoMatch';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Nav from './components/Nav';
+import OrderHistory from './pages/OrderHistory';
+import Success from './pages/Success';
+// import { StoreProvider } from "./utils/GlobalState";
+
+// import Provider making redux store available to any nested components
+import { Provider } from 'react-redux';
+
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <Router>
+        <div>
+          {/* <StoreProvider> */}
+          <Provider store={store}>
+          <Nav />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/signup" component={Signup} />
+            <Route exact path="/orderHistory" component={OrderHistory} />
+            <Route exact path="/products/:id" component={Detail} />
+            <Route exact path="/success" component={Success} />
+            <Route component={NoMatch} />
+          </Switch>
+          {/* </StoreProvider> */}
+          </Provider>
+        </div>
+      </Router>
+    </ApolloProvider>
+  );
+}
+
+export default App;
