6a93b4b2f735482c704cb32174904b1b6c35b9d3
charity.rogers <rogerscl116@gmail.com>
2021-09-17 11:02:50 -0500

    import useDispatch and useSelector to product list component

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -2,17 +2,23 @@ import React, { useEffect } from "react";
 import { useQuery } from '@apollo/client';
 
 import ProductItem from "../ProductItem";
-import { useStoreContext } from "../../utils/GlobalState";
+// import { useStoreContext } from "../../utils/GlobalState";
 import { UPDATE_PRODUCTS } from "../../utils/actions";
 import { QUERY_PRODUCTS } from "../../utils/queries";
 import { idbPromise } from "../../utils/helpers";
 import spinner from "../../assets/spinner.gif"
+import { useDispatch, useSelector } from 'react-redux';
 
 
 function ProductList() {
-  const [state, dispatch] = useStoreContext();
+  // const [state, dispatch] = useStoreContext();
 
   const { currentCategory } = state;
+
+  const state = useSelector((state) => {
+    return state
+  });
+  const dispatch = useDispatch();
 
   const { loading, data } = useQuery(QUERY_PRODUCTS);
 
